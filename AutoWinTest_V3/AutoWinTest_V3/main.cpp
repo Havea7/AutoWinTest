e11e74d1617b355a72cf4dc8400d5adbffecee7e
Havea Daure <63477@student.wsiz.rzeszow.pl>
2020-01-12 01:39:53 +0100

    True first Commit

---
+++ AutoWinTest_V3/AutoWinTest_V3/main.cpp
@@ -0,0 +1,31 @@
+// AutoWinTest.cpp : Defines the entry point for the console application.
+//
+
+//#include <stdafx.h>
+#include <windows.h>
+#include <winuser.h>
+
+#include <iostream>
+
+#include <string>
+
+//using namespace std;
+
+int main()
+{
+	HWND hwnd  = FindWindowA(NULL, "Calculator");
+	UINT hwndval = (UINT)hwnd;
+	std::string hwndstr; //= std::to_string(hwndval);
+	
+	SendMessage(hwnd, WM_CLOSE, NULL, NULL);
+
+	std::cout << hwndstr<< "hwnd\n";
+	//return 0;
+
+	//BOOL EnableWindow(HWND hWnd, BOOL bEnable);
+
+	asfadgagdfg adgadfgfdg
+
+}
+
+ 
